ff51a9b919bce603c8393ea913582b9844482714
Lucas Jovencio <lucasjovencio@outlook.com.br>
2018-08-03 19:53:52 -0300

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -75,8 +75,18 @@ function definiPeso(tipo,id){
 }
 function adicionarFonte(){
     let exeF = "";
+    let exeF1 = "";
+    let exeF2 = "";
+    let exeF3 = "";
+    let exeF4 = "";
+    let exeF5 = "";
     if(id_fonte==0){
         exeF = "tour-step tour-step-four";
+        exeF1 = "tour-step tour-step-five";
+        exeF2 = "tour-step tour-step-six";
+        exeF3 = "tour-step tour-step-seven";
+        exeF4 = "tour-step tour-step-eight";
+        exeF5 = "tour-step tour-step-nine";
     }
     let html = '<div id="fonte-add-'+id_fonte+'"'+ 
         '<div class="pure-g">'+
@@ -93,28 +103,28 @@ function adicionarFonte(){
             '<input id="fonte-valicacao-'+id_fonte+'" type="hidden" value="0">'+
             '<div class="pure-u-1-2 pure-u-md-1-2">'+
                 '<label for="min">Peso da Fonte</label>'+
-                '<input onkeyup="atualizaTempo(2,'+id_fonte+');return false;" id="peso-fonte-'+id_fonte+'"  class="pure-u-23-24" type="number" placeholder="0" min="0" max="20000">'+
+                '<input onkeyup="atualizaTempo(2,'+id_fonte+');return false;" id="peso-fonte-'+id_fonte+'"  class="pure-u-23-24 '+exeF1+'" type="number" placeholder="0" min="0" max="20000">'+
             '</div>'+
             
             '<div class="pure-u-1-2 pure-u-md-1-2">'+
                 '<label for="min">Poder Recebido</label>'+
-                '<input data-mask="000,000" onkeyup="atualizaTempo(3,'+id_fonte+');" data-mask-reverse="true" id="poder-recebido-'+id_fonte+'" class="pure-u-23-24" type="text" step="any" placeholder="000,000">'+
+                '<input data-mask="000,000" onkeyup="atualizaTempo(3,'+id_fonte+');" data-mask-reverse="true" id="poder-recebido-'+id_fonte+'" class="pure-u-23-24 '+exeF2+'" type="text" step="any" placeholder="000,000">'+
             '</div>'+
 
             '<div class="pure-u-1-2 pure-u-md-1-2">'+
                 '<label for="min">Tempo Real</label>'+
-                '<input onkeyup="atualizaTempo(4,'+id_fonte+');" data-mask="00 D 00:00" data-mask-reverse="true" id="tempo-real-'+id_fonte+'" class="pure-u-23-24" type="text" placeholder="30 D 23:55">'+
+                '<input onkeyup="atualizaTempo(4,'+id_fonte+');" data-mask="00 D 00:00" data-mask-reverse="true" id="tempo-real-'+id_fonte+'" class="pure-u-23-24 '+exeF3+'" type="text" placeholder="30 D 23:55">'+
             '</div>'+
             '<div class="pure-u-1-2 pure-u-md-1-2">'+
                 '<label for="min">&emsp;</label>'+
-                '<button class="pure-u-23-24 pure-button" onclick="removerFonte('+id_fonte+');return false;">'+
+                '<button class="pure-u-23-24 pure-button '+exeF4+'" onclick="removerFonte('+id_fonte+');return false;">'+
                     '<i class="fas fa-minus-square"></i>'+
                     'Remover fonte de pontuação'+
                 '</button>'+
             '</div>'+
             '<div class="pure-u-1-2 pure-u-md-1-2">'+
                 '<label for="min">&emsp;</label>'+
-                '<button id="button-check-'+id_fonte+'" class="pure-u-23-24 pure-button" onclick="return false;">'+
+                '<button id="button-check-'+id_fonte+'" class="pure-u-23-24 pure-button '+exeF5+'" onclick="return false;">'+
                     '<i class="fas fa-clock"></i>'+
                 '</button>'+
             '</div>'+
