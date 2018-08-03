682de3f6c6c11789a673a7fa66cb9fcbf0fb89cb
Lucas Jovencio <lucasjovencio@outlook.com.br>
2018-08-03 13:37:03 -0300

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -275,7 +275,7 @@ function atualizaTempo(tipo,id){
     let poderRece = parseFloat($('#poder-recebido-'+id).val().replace(',', '.'));
     let tempoReal = $('#tempo-real-'+id).val();
 
-    if(tipoFonte !=0 && pesoFonte !=null && pesoFonte >0 && (poderRece.length)>3 && tempoReal.length > 6 && pontu_nece > 1 ){
+    if(tipoFonte !=0 && pesoFonte !=null && pesoFonte >0 && (poderRece.length)>3 && tempoReal.length > 6 ){
         console.log('oi');
         calcula_tempo_infernal();
     }
