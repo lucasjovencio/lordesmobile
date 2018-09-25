be8951556fcf690764012ce845406c184574659d
Lucas Jovencio <lucasjovencio@outlook.com.br>
2018-09-25 11:42:32 -0300

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -223,7 +223,7 @@ function definiFormPree(tipo,id_fonte){
             '</div>'+
             '<div class="pure-u-1-1 pure-u-md-1-1">'+
                 '<label for="min">&emsp;</label>'+
-                '<button onclick="openModal('+id_fonte+');" data-id-type="'+id_fonte+'" style="margin-left: 10px;" id="button-information-'+id_fonte+'" class="pure-u-23-24 pure-button '+2+'" onclick="return false;">'+
+                '<button onclick="openModal('+id_fonte+');return false;" data-id-type="'+id_fonte+'" style="margin-left: 10px;" id="button-information-'+id_fonte+'" class="pure-u-23-24 pure-button '+2+'" onclick="return false;">'+
                     '<i class="fas fa-info-circle"></i> Informações Extras Sobre as Tropas.'+
                 '</button>'+
             '</div>';
